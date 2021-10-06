735e2fe48c92b7ad027ce190cb0df1e4d7927246
Stackbit Code Editor <projects@stackbit.com>
2021-10-06 17:03:18 +0000

    init.js: changed

--- static/js/init.js
+++ static/js/init.js
@@ -101,3 +101,18 @@ window.removeAnnouncementHandlers = function() {
 		anncmntClose.removeEventListener('click', announcementCloseHandler, false);
 	}
 }
+
+// animation scroll js
+    var html_body = $('html, body');
+    $('nav li a').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 90
+                }, 1500, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
