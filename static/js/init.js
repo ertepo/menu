4e2387e680806f505b810005f5e4a08c97f966aa
Stackbit Code Editor <projects@stackbit.com>
2021-10-19 19:34:36 +0000

    init.js: changed

--- static/js/init.js
+++ static/js/init.js
@@ -116,3 +116,18 @@ window.removeAnnouncementHandlers = function() {
             }
         }
     });
+
+    // Smooth scrolling for browsers that don't support CSS smooth scrolling
+if (window.getComputedStyle(document.documentElement).scrollBehavior !== 'smooth') {
+    document.querySelectorAll('a[href^="#"]').forEach(internalLink => {
+        const targetElement = document.querySelector(internalLink.getAttribute('href'));
+        if (targetElement) {
+            internalLink.addEventListener('click', (e) => {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                });
+                e.preventDefault();
+            });
+        }
+    });
+}
